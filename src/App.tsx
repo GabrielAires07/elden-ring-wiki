b84de7ce66d8d1c17cc11010eafd2baa392bc4f3
Gabriel Aires <gabriel.aires07@outlook.com>
2026-09-23 13:32:58 -0300

    Estilização de layout e tipografia iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 function App() {
   return (
-    <div>
-      <h1>Elden Ring Wiki</h1>
-      <p>De fã para fã!</p>
+    <div className="min-h-screen bg-neutral-950 text-slate-300 flex flex-col items-center justify-center font-serif"> {/** min-h-screen(min-height) = altura minima */}
+      <div className="border border-amber-900/50 bg-neutral-900/30 p-12 rounded-xl shadow-[0_0_30px_rgba(180,83,9,0.15)] text-center">
+        <h1 className="text-5xl text-amber-500 mb-4 tracking-widest uppercase dropshadow-md">
+          Elden Ring Wiki
+        </h1>
+        <p>
+          De fã para fã!
+        </p>
+      </div>
     </div>
   )
 }
